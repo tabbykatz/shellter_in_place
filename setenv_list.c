0e6b7d1514ed621ae4ecb67be44ed478808fa016
zacwoll <zacwoll@gmail.com>
2020-04-12 04:17:03 +0000

    new branch

--- setenv_list.c
+++ setenv_list.c
@@ -10,30 +10,52 @@ typedef struct env_list
 env_list_t **_initenv_list(void)
 {
 	env_list_t **env;
-	env_list_t *new_env;
+	env_list_t *head = NULL;
+	env_list_t *new_env = NULL;
 	extern char **environ;
 	char *token;
 	int var_count = 0;
 
 	printf("=initenv_list\n");
 
-	*env = new_env;
-
 	for (var_count = 0; environ[var_count]; var_count++)
 	{
 		new_env = malloc(sizeof(env_list_t));
+		if (head)
+			head->next = new_env;
 		token = strdup(strtok(environ[var_count], "="));
 		new_env->NAME = token;
 		token = strdup(strtok(NULL, " "));
 		new_env->VALUE = token;
-		new_env = new_env->next;
+		new_env->next = NULL;
+		head = new_env;
+		if (var_count == 0)
+			*env = head;
 	}
-	
-	
+
 	return env;
+}
+
+void printenv_list(env_list_t **env)
+{
+	env_list_t *i = *env;
+
+	printf("=printenv_list\n");
+	while (i)
+	{
+		printf("%s", i->NAME);
+		printf("=");
+		printf("%s\n", i->VALUE);
+		i = i->next;
+	}
 }
 
 int main(void)
 {
 	env_list_t **env = _initenv_list();
+	
+	printf("%s\n", (*env)->NAME);
+
+	printenv_list(env);
+	return (0);
 }
