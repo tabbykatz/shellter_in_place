c8ec9b3336264b25d89d75535786c00b9513b52d
zacwoll <zacwoll@gmail.com>
2020-04-13 20:14:10 +0000

    removed cp

--- cd.c
+++ cd.c
@@ -8,12 +8,11 @@
 void _cd(char **argv, env_list_t **env)
 {
 	char buf[BUFSIZ]; /* BUFSIZ is predefined~8k */
-	char *cp;
 	char *home = _getenv_list_value("HOME", env);
 	char *OLDPWD[4] = {"", "OLDPWD", buf, NULL};
 	char *PWD[4] = {"", "PWD", buf, NULL};
 
-	cp = getcwd(buf, sizeof(buf));
+	getcwd(buf, sizeof(buf));
 	printf("Current dir: %s\n", buf);
 
 	printf("Changing to %s\n", argv[1]);
@@ -25,7 +24,7 @@ void _cd(char **argv, env_list_t **env)
 
 	_setenv_list(OLDPWD, env);
 
-	cp = getcwd(buf, sizeof(buf));
+	getcwd(buf, sizeof(buf));
 	printf("Current dir is now: %s, %lu bytes\n", buf, strlen(buf));
 	_setenv_list(PWD, env);
 }
