0c52e0f37d2a368d76fffa20ef2c02728d87e42e
zacwoll <zacwoll@gmail.com>
2020-04-13 00:46:31 +0000

    added strchr again

--- helpers2.c
+++ helpers2.c
@@ -42,5 +42,27 @@ char *_strcat(char *dest, char *src)
 		dest++;
 	}
 	*dest = '\0';
-return (begin);
+	return (begin);
+}
+
+/**
+  * _strchr - locates a character
+  * @s: the string we look thru
+  * @c: the character we look for
+  * Return: a pointer to the char or NULL
+  */
+char *_strchr(char *s, char c)
+{
+	if (s == NULL)
+		return (NULL);
+	for (; *s; s++)
+	{
+		if (*s == c)
+			return (s);
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (NULL);
 }
